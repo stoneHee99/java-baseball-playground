cfe1c12a0f56048eab62f71da8677ef335421468
StoneHee99 <kiit0901@gmail.com>
2024-04-18 13:47:32 +0900

    implement requirements2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -7,6 +7,8 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -25,6 +27,13 @@ public class SetTest {
     void size() {
         int size = numbers.size();
         assertThat(size).isEqualTo(3);
+    }
+
+    @DisplayName("set의 contains 메서드가 포함된 값이 있으면 true를 반환하는지")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int num) {
+        assertThat(numbers.contains(num)).isTrue();
     }
 
 
