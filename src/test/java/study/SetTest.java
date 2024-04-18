ab1d16d31cb7d6e20df5f36fbe5d60410381f746
StoneHee99 <kiit0901@gmail.com>
2024-04-18 10:50:44 +0900

    implement set test and requirement1

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,31 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("set의 size 메서드가 size의 크기를 잘 반환하는지")
+    @Test
+    void size() {
+        int size = numbers.size();
+        assertThat(size).isEqualTo(3);
+    }
+
+
+}
